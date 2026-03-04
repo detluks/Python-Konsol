8bcf9f3bee1d609675dacc1e4a8614c7acee3693
detluks <lukas@mirweb.dk>
2026-03-04 09:31:24 +0100

    tilføjet password og kryptering

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import requests
-ip = input("indsæt ip'en lil bro: ")
-startURL = f"http://{ip}:8000"
+import hashlib
+startURL = "http://127.0.0.1:8000"
 
 def login():
     url = f"{startURL}/users"
     while True:
-        user = input("Username: ")     
-        respone = requests.post(url, json={"username": user})
+        user = input("Username: ")
+        hash = hashlib.md5()
+        hash.update(user.encode('utf-8'))    
+        respone = requests.post(url, json={"username": hash.hexdigest()})
+        result = respone.json()
+        if result["status"] == "exists":
+            break
+    url = f"{startURL}/passwords"
+    while True:
+        Pass = input("Password: ")
+        hash = hashlib.md5()
+        hash.update(Pass.encode('utf-8'))
+        respone = requests.post(url, json={"password": hash.hexdigest()})
         result = respone.json()
         if result["status"] == "loggedIn":
-            return user
-            
-
+            break
+    return user
 
 class User:
     def __init__(self, name):
         self.name = name
 
-
 user = User(login())
-print(f"du blev logget ind som:{user} lil bro")
+print(f"du blev logget ind som:{user.name} lil bro")
--- server.py
+++ server.py
@@ -2,13 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+users = {"79af0c177db2ee64b7301af6e1d53634": {"password":"70375478134bc7187a0d5a0ffd59c283","admin?":False},"9c5ddd54107734f7d18335a5245c286b":{"password":"0f35b3151769db9b184ade7ee5eed39c","admin?":True}}
+user = ""
+
 @app.post('/users')
-def getuser(user:dict):
-    if user["username"] == "ham":
-        return {"status": "loggedIn"}
+def getuser(u:dict):
+    global user
+    if users.__contains__(u["username"]):
+        user = u["username"]
+        return {"status": "exists"}
     return {"status": ""}
 
-
+@app.post('/passwords')
+def getuser(Pass:dict):
+    if Pass["password"] == users[user]["password"]:
+        return {"status": "loggedIn"}
+    return {"status": ""}
 
 
 if __name__ == "__main__":
